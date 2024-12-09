677ab62a6b39721c4ea7389b516663757d839dec
AJI Suryawan <ajisuryawan123@gmail.com>
2024-12-09 16:39:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/Calculator.java
@@ -0,0 +1,59 @@
+package com.example.kalkulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calculator extends AppCompatActivity {
+
+    TextView tvName;
+    Spinner spOperator;
+    Button btnCalculate;
+    int indexSpinnerOp = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calculator);
+        tvName = (TextView) findViewById(R.id.tvName);
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        spOperator = (Spinner) findViewById(R.id.spOperator);
+        spOperator.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int indexSpinner, long l) {
+                indexSpinnerOp = indexSpinner;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // kondisi switch
+                switch (indexSpinnerOp){
+                    case 0:
+                        // tambah
+                        // parsing data dari edittext "5" -> 5
+                        break;
+                }
+            }
+        });
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("nama");
+        tvName.setText("welcome "+name);
+    }
+}
---
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.kalkulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edUsername;
+    EditText edPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this , Calculator.class);
+                in.putExtra("nama",edUsername.getText().toString());
+                startActivity(in);
+            }
+        });
+
+    }
+}
